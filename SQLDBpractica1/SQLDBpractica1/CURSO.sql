4738272bbe1d4243e25bd71734c9cc173104030d
sneider <oparra3@udi.edu.co>
2021-09-30 16:38:40 -0500

    actualizacion

--- SQLDBpractica1/SQLDBpractica1/CURSO.sql
+++ SQLDBpractica1/SQLDBpractica1/CURSO.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[CURSO]
 (
-	[Cod] INT NOT NULL PRIMARY KEY, 
+	[Cod] INT NOT NULL PRIMARY KEY IDENTITY (1,1), 
     [Nombre] NVARCHAR(50) NOT NULL, 
     [Fecha_inicio] NVARCHAR(50) NOT NULL, 
     [Duracion] INT NOT NULL, 
