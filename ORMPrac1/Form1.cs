831a44be85064de4468f5a436f0ca6542370823f
sneider <oparra3@udi.edu.co>
2021-09-30 22:34:23 -0500

    ORM

--- ORMPrac1/Form1.cs
+++ ORMPrac1/Form1.cs
@@ -12,9 +12,103 @@ namespace ORMPrac1
 {
     public partial class Form1 : Form
     {
+        public List<Model.ALUMNO> oAlumno;
+        public List<Model.APODERADO> oApoderado;
+        public List<Model.CURSO> oCurso;
+        public List<Model.INSCRITO> oInscrito;
+        int indice = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //se llena el combobox
+            comboBox1.Items.Add("ALUMNO");
+            comboBox1.Items.Add("APODERADO");
+            comboBox1.Items.Add("CURSO");
+            comboBox1.Items.Add("INSCRITO");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            using (Model.DBPractica1Entities db = new Model.DBPractica1Entities())
+            {
+                switch(comboBox1.SelectedIndex)
+                {
+                    case 0:
+                        oAlumno = db.ALUMNO.ToList();
+                        break;
+                    case 1:
+                        oApoderado = db.APODERADO.ToList();
+                        break;
+                    case 2:
+                        oCurso = db.CURSO.ToList();
+                        break;
+                    case 3:
+                        oInscrito = db.INSCRITO.ToList();
+                        break;
+                }
+
+            }
+
+            indice = 0;
+            Llenar();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            indice--;
+            Llenar();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            indice++;
+            Llenar();
+        }
+
+        public void Llenar()
+        {
+            if (indice < 0)
+                indice = 0;
+
+            string cadena = "";
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    if (indice >= oAlumno.Count)
+                        indice = oAlumno.Count - 1;
+                    cadena = oAlumno[indice].Id.ToString() + "," + oAlumno.[indice].Nombre + ", de" + oAlumno[indice].Ciudad + "," + oAlumno[indice].Edad + "años";
+                    break;
+                case 1:
+                    if (indice >= oApoderado.Count)
+                        indice = oApoderado.Count - 1;
+
+                    using (Model.DBPractica1Entities db = new Model.DBPractica1Entities())
+                    {
+                        oAlumno = db.ALUMNO.ToList();
+                        cadena = oApoderado[indice].Id.ToString() + "," + oApoderado[indice].Nombre + ", es de el|la apoderado|a de " + oAlumno.Find(a => a.Id == (int)oApoderado[indice].Id_Alumno).Nombre;
+
+
+                    }
+                    break;
+
+                case 2:
+                    if (indice >= oCurso.Count)
+                        indice = oCurso.Count - 1;
+                    break;
+
+                case 3:
+                    if (indice >= oInscrito.Count)
+                        indice = oInscrito.Count - 1;
+                    break;
+
+            }
+            textBox1.Text = cadena;
+
         }
     }
 }
