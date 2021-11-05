0b8bf07f873fd009e66f984b90672241c3f2cc28
sneider <oparra3@udi.edu.co>
2021-11-04 20:51:09 -0500

    creo BDMercamax

---
+++ BDMercamax/Producto.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Producto]
+(
+	[Id_producto] INT NOT NULL PRIMARY KEY, 
+    [nombre_producto] NVARCHAR(MAX) NOT NULL, 
+    [fecha_vencimiento] DATE NOT NULL, 
+    [barcode] DATE NOT NULL, 
+    [precio] DECIMAL(18, 2) NOT NULL, 
+    [nit] INT NOT NULL, 
+    [id_tipo] INT NOT NULL
+)
