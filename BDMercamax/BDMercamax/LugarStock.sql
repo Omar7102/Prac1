a0bb986f3374ee1eb111fb7c83eff73cf97a9f32
sneider <oparra3@udi.edu.co>
2021-11-17 14:54:18 -0500

    subo BDMercamax de nuevo

---
+++ BDMercamax/BDMercamax/LugarStock.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[LugarStock]
+(
+	[id_lugar] INT NOT NULL PRIMARY KEY, 
+    [barcode_producto] INT NOT NULL UNIQUE, 
+    [cantidad_bodega] INT NOT NULL, 
+    [cantidad_gondola] INT NOT NULL, 
+    [sección_bodega] NVARCHAR(MAX) NOT NULL, 
+    [sección_gondola] NVARCHAR(MAX) NOT NULL
+)
