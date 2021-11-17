3dc0c5bd9ccee0097b916b2cf3386c2c8c173a49
sneider <oparra3@udi.edu.co>
2021-11-17 14:58:07 -0500

    elimino de nuevo BDMercamax

--- BDMercamax/BDMercamax/Venta.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Venta]
-(
-	[id_venta] INT NOT NULL PRIMARY KEY, 
-    [barcode_producto] INT NOT NULL UNIQUE, 
-    [cantidad] INT NOT NULL, 
-    [id_factura] INT NOT NULL, 
-    CONSTRAINT [FK_Venta_ToTable] FOREIGN KEY ([id_factura]) REFERENCES [Facturacion]([id_factura])
-)
