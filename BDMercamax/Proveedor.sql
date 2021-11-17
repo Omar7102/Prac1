fc2713d1d486176aada45fbfc48bd0fe010386c8
sneider <oparra3@udi.edu.co>
2021-11-17 11:46:07 -0500

    Elimino BDMercamax

--- BDMercamax/Proveedor.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Proveedor]
-(
-	[nit] INT NOT NULL PRIMARY KEY, 
-    [nombre_proveedor] NVARCHAR(MAX) NOT NULL, 
-    [telefono_proveedor] NVARCHAR(MAX) NOT NULL, 
-    [email_proveedor] NVARCHAR(MAX) NOT NULL, 
-    [direccion_proveddor] NVARCHAR(MAX) NOT NULL, 
-    [agente_ventas] NVARCHAR(MAX) NOT NULL
-)
--- BDMercamax/Venta.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Venta]
-(
-	[id_venta] INT NOT NULL PRIMARY KEY, 
-    [barcode_producto] INT NOT NULL UNIQUE, 
-    [cantidad] INT NOT NULL, 
-    [id_factura] INT NOT NULL
-)
